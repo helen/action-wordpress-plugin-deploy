64424d6b65614a78adabd89e3b3318df97d406b0
Clayton Collie <clayton.collie@gmail.com>
2024-08-14 09:31:31 -0500

    Change syntax to dir-merge to see if direcotires are supported in the distignore file

--- deploy.sh
+++ deploy.sh
@@ -96,7 +96,7 @@ if [[ "$BUILD_DIR" = false ]]; then
 		# Copy from current branch to /trunk, excluding dotorg assets
 		# The --filter flag will allow the full .gitignore syntax to be used in .distignore
 		# The --delete flag will delete anything in destination that no longer exists in source
-		rsync -rc --filter="merge $GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
+		rsync -rc --filter="dir-merge,- $GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
 	else
 		echo "ℹ︎ Using .gitattributes"
 
