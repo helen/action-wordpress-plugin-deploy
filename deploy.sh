3509cb4a6ffa594a3b6e1e56ea37bd256d18f9f6
Clayton Collie <clayton.collie@gmail.com>
2024-09-16 08:34:13 -0500

    process file again

--- deploy.sh
+++ deploy.sh
@@ -98,9 +98,9 @@ if [[ "$BUILD_DIR" = false ]]; then
 		# The --filter flag will allow the full .gitignore syntax to be used in .distignore
 		# The --delete flag will delete anything in destination that no longer exists in source
 		# rsync -rc --filter="dir-merge,- $GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
-		sed 's/^/- /' "$GITHUB_WORKSPACE/.distignore" | sed 's/^- !/+ /' > tmp_rules && \
-		rsync -rc --filter="merge tmp_rules" "$GITHUB_WORKSPACE/" trunk/ --delete && \
-		rm tmp_rules
+		sed 's/^!/+ /' "$GITHUB_WORKSPACE/.distignore" | sed 's/^[^+]/- /' > tmp_rules && \
+		rsync -rcv --filter="merge tmp_rules" "$GITHUB_WORKSPACE/" trunk/ --delete --itemize-changes && \
+		rm tmp_rules	
 	else
 		echo "ℹ︎ Using .gitattributes"
 
