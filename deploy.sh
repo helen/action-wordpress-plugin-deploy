aaec0a2090dd18d52081906be5889e7cdb24bab4
Clayton Collie <clayton.collie@gmail.com>
2024-09-16 07:40:44 -0500

    add logging to rsync command

--- deploy.sh
+++ deploy.sh
@@ -96,7 +96,8 @@ if [[ "$BUILD_DIR" = false ]]; then
 		# Copy from current branch to /trunk, excluding dotorg assets
 		# The --filter flag will allow the full .gitignore syntax to be used in .distignore
 		# The --delete flag will delete anything in destination that no longer exists in source
-		rsync -rc --filter="dir-merge,- $GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
+		# rsync -rc --filter="dir-merge,- $GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
+		rsync -rcv --filter="dir-merge,- $GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded --itemize-changes
 	else
 		echo "ℹ︎ Using .gitattributes"
 
